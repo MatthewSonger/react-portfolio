56f598f8c826af57b9184ee6cc6cfb8da48d87bd
Matthew Songer <matt91893@gmail.com>
2022-07-03 21:20:38 -0400

    add import react to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { render } from 'react-dom'
-import App from './App'
-import { ThemeProvider } from './contexts/theme'
-import './index.css'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
+import { ThemeProvider } from './contexts/theme';
+import './index.css';
 
 render(
   <ThemeProvider>
